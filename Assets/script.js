0116c5f141a2ca4cfafb6985315f9b8daba38085
rodolfopoly <rodolfopoly@gmail.com>
2022-04-08 19:57:48 -0400

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,43 @@
+var myQuestions = [
+    {
+      question: "At 20 minutes into the game, Roshan will have how much maximum HP?",
+      answers: {
+        a: '8000',
+        b: '9000',
+        c: '10000',
+        d: '15000',
+      },
+      correctAnswer: 'c'
+    },
+    {
+      question: "When can you upgrade the Courier?",
+      answers: {
+        a: 'At minute 2:00',
+        b: 'At minute 3:00',
+        c: 'At minute 4:00',
+        d: 'At the start of the game';
+      },
+      correctAnswer: 'b'
+    },
+    {
+      question:"Which Strength Hero has the highest Strength gain per level?",
+      answers:{
+        a:'Centaur',
+        b:'Treant',
+        c:'Doom',
+        d:'Pudge',
+      },
+      correctAnswer: 'a'
+    },
+    {
+        question:"How many melees and ranged creeps spawn in the first wave, for only one lane? Not counting the enemy creeps in that lane!",
+        answers:{
+          a:'4 melee creeps, 1 ranged creep',
+          b:'3 melee creeps, 1 ranged creep',
+          c:'4 melee creeps, 2 ranged creeps',
+          d:'3 melee creeps, 2 ranged creeps',
+        },
+        correctAnswer:'b'
+      },
+  ];
+  
